44f83ce6a3fe181f29bc6feb65193dde9c10f947
Zamaria Winston <112509733+zswt64@users.noreply.github.com>
2022-11-02 16:12:59 -0400

    fic\xes
    fixes

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'JsebejmwUBeWwUPd0X8YPyxqe',
+  consumer_secret:      'JsebejmwUBeWwUPd0X8YPyxqe',
+  access_token:         'bhUTm5eauXlQyBmAVCZz3QN3QJOlCEmklI0qk4hal96kkd9txL',
+  access_token_secret:  'AAAAAAAAAAAAAAAAAAAAAEy9igEAAAAAz3uDbx4KdMyzTsuqR8BahBJE1CY%3DSDAEUqLYYga5g5kVmGsdZXppqBEzw0ZgMKxjRwR4N3HgTqgQQ7'
+}
