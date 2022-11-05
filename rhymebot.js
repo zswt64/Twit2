415b10340c6888fb8afa56fd9a903cedeb1be023
jlight30 <112646399+jlight30@users.noreply.github.com>
2022-11-04 22:51:14 -0400

    Add files via upload

---
+++ rhymebot.js
@@ -0,0 +1,87 @@
+// Our Twitter library
+const { request } = require('express');
+var Twit = require('twit');
+
+
+// We need to include our configuration file
+var T = new Twit(require('./config.js'));
+
+
+
+
+var param = {q: "#barbz", count: 60, result_type: "recent"};
+
+
+
+var url1 = "https://api.wordnik.com/v4/word.json/"
+"https://api.wordnik.com/v4/word.json/orange/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=5n9terp6ususoehfh36a8cke0yfdvroprv6ww48m4osl5pm14"
+var url2 = "/relatedWords?useCanonical=false&relationshipTypes=rhyme&limitPerRelationshipType=10&api_key=5n9terp6ususoehfh36a8cke0yfdvroprv6ww48m4osl5pm14"
+
+
+
+
+
+
+
+  
+
+
+function tweetMe(word) {
+  T.get('search/tweets', param, function (error, data) {
+    num = Math.random(60)
+    console.log(num);
+    // log out any errors and responses
+    console.log(error, data);
+    // If our search request to the server had no errors...
+    if (!error) {
+      var retweetmessage = data.statuses;
+      var retweetdata = data.statuses
+          .filter(retweetdat => retweetdat["lang"] == "en")
+          .map(retweetdat => retweetdat.text)
+      let imp_Info = [];
+      for (var i = 30; i < 44; i++) {
+        imp_Info.push(retweetdata[i]);
+      }
+      console.log(imp_Info);
+      var message = "Here's a cool rhyme: " + word + " " + imp_Info[0];
+      T.post("statuses/update", {status: message}, function(error, tweet, response) {
+        if (error) {
+            console.log(error);
+        }
+      });
+    //}
+    }
+    });
+}
+
+
+
+function runStuff(wordToGrab) {
+
+
+  fetch(url1 + wordToGrab + url2)
+  .then(res => res.json())
+  .then(out => {
+    var temp;
+    temp = out[0]["words"];
+    console.log(temp);
+
+    tweetMe(temp);
+
+
+  });
+
+
+}
+runStuff("shark");
+
+//whatever word we are rhyming with gets passed into main function
+
+
+
+
+
+
+// ...and then every hour after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
+setInterval(runStuff, 1000 * 60 * 60);
